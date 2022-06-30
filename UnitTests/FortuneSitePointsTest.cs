055b012181adcdfb15c28df90bd563f9531cadac
RudyTheDev <3857299+RudyTheDev@users.noreply.github.com>
2022-06-30 16:46:45 +0300

    Add a unit test for FortuneSite's Points

---
+++ UnitTests/FortuneSitePointsTest.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using VoronoiLib;
+using VoronoiLib.Structures;
+
+namespace UnitTests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.Self)]
+    public class FortuneSitePointsTest
+    {
+        [Test]
+        public void ThreeFortuneSites()
+        {
+            List<FortuneSite> points = new List<FortuneSite> {new FortuneSite(100, 100), new FortuneSite(200, 200), new FortuneSite(200, 150)};
+            FortunesAlgorithm.Run(points, 0, 0, 600, 600);
+
+            // Site 1
+            FortuneSite site = points[0];
+            List<VPoint> corners = site.Points;
+            Assert.NotNull(corners);
+            Assert.AreEqual(3, corners.Count);
+            Assert.AreEqual(212.5, corners[0].X);
+            Assert.AreEqual(0, corners[0].Y);
+            Assert.AreEqual(125, corners[1].X);
+            Assert.AreEqual(175, corners[1].Y);
+            Assert.AreEqual(0, corners[2].X);
+            Assert.AreEqual(300, corners[2].Y);
+
+            // Site 2
+            site = points[1];
+            corners = site.Points;
+            Assert.NotNull(corners);
+            Assert.AreEqual(3, corners.Count);
+            Assert.AreEqual(125, corners[0].X);
+            Assert.AreEqual(175, corners[0].Y);
+            Assert.AreEqual(600, corners[1].X);
+            Assert.AreEqual(175, corners[1].Y);
+            Assert.AreEqual(0, corners[2].X);
+            Assert.AreEqual(300, corners[2].Y);
+
+            // Site 3
+            site = points[2];
+            corners = site.Points;
+            Assert.NotNull(corners);
+            Assert.AreEqual(3, corners.Count);
+            Assert.AreEqual(212.5, corners[0].X);
+            Assert.AreEqual(0, corners[0].Y);
+            Assert.AreEqual(600, corners[1].X);
+            Assert.AreEqual(175, corners[1].Y);
+            Assert.AreEqual(125, corners[2].X);
+            Assert.AreEqual(175, corners[2].Y);
+        }
+    }
+}
