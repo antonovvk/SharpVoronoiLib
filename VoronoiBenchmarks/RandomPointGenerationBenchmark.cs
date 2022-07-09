d65ecc11dd3c50f9ba8beaa351a2e3822a57b1c5
RudyTheDev <3857299+RudyTheDev@users.noreply.github.com>
2022-07-09 17:12:06 +0300

    Add BenchmarkDotNet package to speed test project and rename it VoronoiBenchmarks; remove manual speed test; add a benchmark test for random point generation

---
+++ VoronoiBenchmarks/RandomPointGenerationBenchmark.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+
+namespace SharpVoronoiLib.Benchmarks
+{
+    public class RandomPointGenerationBenchmark
+    {
+        [Benchmark]
+        public List<VoronoiSite> Uniform()
+        {
+            VoronoiPlane plane = new VoronoiPlane(0, 0, 1000, 1000);
+            return plane.GenerateRandomSites(10000, PointGenerationMethod.Uniform);
+        }
+
+        [Benchmark]
+        public List<VoronoiSite> Gaussian()
+        {
+            VoronoiPlane plane = new VoronoiPlane(0, 0, 1000, 1000);
+            return plane.GenerateRandomSites(10000, PointGenerationMethod.Gaussian);
+        }
+    }
+}
